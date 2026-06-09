28d2dda4275f99cbb82253c8fe1ee3917341bc62
yuczhang <yuczhang@f0r9s18.paris.42.school>
2026-06-09 19:22:20 +0200

    dfs“

--- incs/network_h/ServerSocket.hpp
+++ incs/network_h/ServerSocket.hpp
@@ -6,11 +6,44 @@
 /*   By: yuczhang <yuczhang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/07 22:40:30 by yuczhang          #+#    #+#             */
-/*   Updated: 2026/06/07 22:40:57 by yuczhang         ###   ########.fr       */
+/*   Updated: 2026/06/09 19:21:39 by yuczhang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVERSOCKET_HPP
 #define SERVERSOCKET_HPP
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string>
+#include <exception>
+
+class ServerSocket
+{
+	private:
+		int					_fd;
+		int					_port;
+		std::string			_host;
+		struct sockaddr_in	_adress;
+	
+		ServerSocket(const ServerSocket& other);
+		ServerSocket&	operator=(const ServerSocket& other);
+	public:
+		ServerSocket(int port, const std::string& host = "0.0.0.0");
+		~ServerSocket();
+
+		void	init();
+		int		getFD() const;
+
+		class SocketException : public std::exception
+		{
+			private:
+				std::string _msg;
+			public:
+				SocketException(const std::string& msg) : _msg(msg) {}
+				virtual ~SocketException() throw() {}
+				virtual const char* what() const throw() { return _msg.c_str(); }
+		};
+};
 
 #endif
