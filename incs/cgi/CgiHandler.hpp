b39b1471f087ea0c9eb5e6bde1bbf0024abe56f5
Rongyan <rozhang@f6r3s1.paris.42.school>
2026-07-07 19:16:11 +0200

    cgi part architecture done

--- incs/cgi/CgiHandler.hpp
+++ incs/cgi/CgiHandler.hpp
@@ -1,1 +1,41 @@
-#
+#ifndef CGIHANDLER_HPP
+# define CGIHANDLER_HPP
+
+# include <iostream>
+# include <string>
+# include <map>
+# include <unistd.h>
+
+struct	CgiRequest
+{
+	std::string							method;
+	std::string							scriptPath;
+	std::string							queryString;
+	std::string							httpBody;
+	std::map<std::string, std::string>	headerInfo;
+};
+
+class	CgiHandler
+{
+private:
+	char**	_envp; //hold the dynamically allocated env variables
+	char**	_argv; //hold the argument for execve;
+	int		_pipe_in[2]; //reading
+	int		_pipe_out[2]; //writing
+	pid_t	_pid; //to store the process id of the child clone so the parent can wait for it later
+
+	void	_buildEnvp(const CgiRequest& req);
+	void	_buildArgv(const CgiRequest& req);
+	void	_freeArray(char** array);
+
+public:
+	CgiHandler();
+	CgiHandler(const CgiHandler& other);
+	CgiHandler& operator=(const CgiHandler& other);
+	~CgiHandler();
+
+	std::string	execute(const CgiRequest& req); //public interface aka entry point, std::string is the HTML result
+};
+
+
+#endif
--- srcs/cgi/CgiHandler.cpp
+++ srcs/cgi/CgiHandler.cpp
@@ -1,1 +1,57 @@
-#
+#include "CgiHandler.hpp"
+
+CgiHandler::CgiHandler() : _envp(NULL), _argv(NULL), _pid(-1)
+{
+	_pipe_in[0] = -1;
+	_pipe_in[1] = -1;
+	_pipe_out[0] = -1;
+	_pipe_out[1] = -1;
+}
+
+CgiHandler::CgiHandler(const CgiHandler& other)
+{
+	*this = other;
+}
+
+CgiHandler& CgiHandler::operator=(const CgiHandler& other)
+{
+	if (this != &other)
+	{
+		_pid = other._pid;
+		_pipe_in[0] = other._pipe_in[0];
+		_pipe_in[1] = other._pipe_in[1];
+		_pipe_out[0] = other._pipe_out[0];
+		_pipe_out[1] = other._pipe_out[1];
+	}
+	return *this;
+}
+
+CgiHandler::~CgiHandler()
+{
+	_freeArray(_envp);
+	_freeArray(_argv);
+
+	if (_pipe_in[0] != -1) 
+		close(_pipe_in[0]);
+	if (_pipe_in[1] != -1) 
+		close(_pipe_in[1]);
+	if (_pipe_out[0] != -1) 
+		close(_pipe_out[0]);
+	if (_pipe_out[1] != -1) 
+		close(_pipe_out[1]);
+}
+
+void	CgiHandler::_freeArray(char ** array)
+{
+	int i;
+
+	if (!array)
+		return ;
+	i = 0;
+	while (array[i] != NULL)
+	{
+		delete[] array[i];
+		i++;
+	}
+	delete[] array;
+}
